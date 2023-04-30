71d95ee713e3c67abe402acb615a71669b5717c2
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2023-04-30 18:23:07 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.suggestionsListBox = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // suggestionsListBox
+            // 
+            this.suggestionsListBox.FormattingEnabled = true;
+            this.suggestionsListBox.Location = new System.Drawing.Point(12, 12);
+            this.suggestionsListBox.Name = "suggestionsListBox";
+            this.suggestionsListBox.Size = new System.Drawing.Size(155, 134);
+            this.suggestionsListBox.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(118, 218);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(49, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(63, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(118, 297);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 44);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "6";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(8, 347);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 44);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "7";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(118, 347);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 44);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "9";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(63, 297);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 44);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "5";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(63, 347);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(49, 44);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(63, 397);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(49, 44);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(8, 397);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(49, 44);
+            this.button11.TabIndex = 3;
+            this.button11.Text = "*";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(118, 397);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(49, 44);
+            this.button12.TabIndex = 3;
+            this.button12.Text = "#";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(69, 207);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(37, 31);
+            this.button13.TabIndex = 3;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(35, 179);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(37, 31);
+            this.button14.TabIndex = 3;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(101, 179);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(37, 31);
+            this.button15.TabIndex = 3;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(69, 154);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(37, 31);
+            this.button16.TabIndex = 3;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(8, 215);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(49, 23);
+            this.button17.TabIndex = 2;
+            this.button17.Text = "C";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 450);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.suggestionsListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox suggestionsListBox;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+    }
+}
+
---
+++ WindowsFormsApp16/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+        private string currentLanguage = "en";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadDictionary(currentLanguage);
+        }
+
+        private void LoadDictionary(string language)
+        {
+            dictionary.Clear();
+
+            string filePath = $"{language}_dictionary.txt";
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            string[] parts = line.Split(' ');
+
+                            if (parts.Length == 2)
+                            {
+                                string word = parts[0];
+                                string key = parts[1];
+
+                                if (dictionary.ContainsKey(key))
+                                {
+                                    dictionary[key].Add(word);
+                                }
+                                else
+                                {
+                                    dictionary[key] = new List<string> { word };
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateSuggestions(string text)
+        {
+            List<string> suggestions = GetSuggestions(text);
+
+            if (suggestions.Count > 0)
+            {
+                suggestionsListBox.DataSource = suggestions;
+                suggestionsListBox.Visible = true;
+            }
+            else
+            {
+                suggestionsListBox.Visible = false;
+            }
+        }
+
+        private List<string> GetSuggestions(string text)
+        {
+            List<string> suggestions = new List<string>();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] keys = text.Split(' ');
+
+                if (keys.Length > 0)
+                {
+                    string lastKey = keys[keys.Length - 1];
+
+                    if (dictionary.ContainsKey(lastKey))
+                    {
+                        suggestions = dictionary[lastKey];
+                    }
+                }
+            }
+
+            return suggestions;
+        }
+
+        private void AddToDictionary(string word)
+        {
+            string key = GetKeyForWord(word);
+
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key].Add(word);
+            }
+            else
+            {
+                dictionary[key] = new List<string> { word };
+            }
+
+            SaveDictionary(currentLanguage);
+        }
+
+        private string GetKeyForWord(string word)
+        {
+            string key = "";
+
+            foreach (char c in word)
+            {
+                key += GetKeyForCharacter(c);
+            }
+
+            return key;
+        }
+        private string GetKeyForCharacter(char c)
+        {
+            switch (currentLanguage)
+            {
+                case "en":
+                    if (c >= 'a' && c <= 'c') return "2";
+                    if (c >= 'd' && c <= 'f') return "3";
+                    if (c >= 'g' && c <= 'i') return "4";
+                    if (c >= 'j' && c <= 'l') return "5";
+                    if (c >= 'm' && c <= 'o') return "6";
+                    if (c >= 'p' && c <= 's') return "7";
+                    if (c >= 't' && c <= 'v') return "8";
+                    if (c >= 'w' && c <= 'z') return "9";
+                    break;
+
+                case "ru":
+                    if (c == 'а' || c == 'б' || c == 'в') return "2";
+                    if (c == 'г' || c == 'д' || c == 'е') return "3";
+                    if (c == 'ж' || c == 'з' || c == 'и') return "4";
+                    if (c == 'й' || c == 'к' || c == 'л') return "5";
+                    if (c == 'м' || c == 'н' || c == 'о') return "6";
+                    if (c == 'п' || c == 'р' || c == 'с') return "7";
+                    if (c == 'т' || c == 'у' || c == 'ф') return "8";
+                    if (c == 'х' || c == 'ц' || c == 'ч') return "9";
+                    if (c == 'ш' || c == 'щ' || c == 'ъ') return "0";
+                    if (c == 'ы' || c == 'ь' || c == 'э') return "1";
+                    if (c == 'ю' || c == 'я') return "2";
+                    break;
+            }
+
+            return "";
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) || e.KeyChar == ' ')
+            {
+                if (e.KeyChar == ' ')
+                {
+                    suggestionsListBox.Items.Add(" ");
+                }
+                else
+                {
+                    suggestionsListBox.Items.Add(e.KeyChar.ToString());
+                }
+
+                string digits = "";
+                foreach (string item in suggestionsListBox.Items)
+                {
+                    digits += item;
+                }
+
+                List<string> words = FindWords(digits);
+
+                suggestionsListBox.Items.Clear();
+
+                foreach (string word in words)
+                {
+                    suggestionsListBox.Items.Add(word);
+                }
+            }
+        }
+        private List<string> FindWords(string digits)
+        {
+            List<string> words = new List<string>();
+
+            string[] arr = digits.Trim().Split(' ');
+
+            foreach (string s in arr)
+            {
+                if (dictionary.ContainsKey(s))
+                {
+                    words.Add(arr[]);
+                }
+            }
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string word = txtInput.Text;
+            if (string.IsNullOrEmpty(word))
+            {
+                MessageBox.Show("Введите слово для добавления в словарь");
+                return;
+            }
+
+            if (!dictionary.ContainsKey(word))
+            {
+                dictionary.Add(word, true);
+                MessageBox.Show($"Слово \"{word}\" успешно добавлено в словарь");
+            }
+            else
+            {
+                MessageBox.Show($"Слово \"{word}\" уже есть в словаре");
+            }
+        }
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonAdd_Click(sender, e);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        
+    }
+}
